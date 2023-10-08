7a88d6c8da6fc65428195396155ebd2df0478832
Graziani Zanfolin <54035931+zanfolin@users.noreply.github.com>
2023-10-08 12:00:36 +0000

    First Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { GluestackUIProvider, Text, Box, config } from '@gluestack-ui/themed';
+
+export default function App() {
+  return (
+    <GluestackUIProvider config={config.theme}>
+      <Box width="100%" justifyContent="center" alignItems="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
+    </GluestackUIProvider>
+  );
+}
